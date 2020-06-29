a81e80967a72647f2a9821c01861c7a81e9eddd1
wparedes <wparedesgt@gmail.com>
2020-06-29 11:35:59 -0600

    Ejercicio No. 4 y examen

---
+++ analisis/Ejercicio_05.R
@@ -0,0 +1,10 @@
+#Ejercicio_05
+
+library(swirl)
+
+#Espectativas
+
+#Expectativas. (Las diapositivas para este y otros cursos de Ciencia de datos se pueden encontrar en github https://github.com/DataScienceSpecialization/courses/. Si desea utilizarlos, debe descargarlos como un archivo zip y verlos localmente. Esta lección corresponde a 06_Statistical_Inference/04_Expectations.)
+
+#En esta lección, como es de esperar, discutiremos los valores esperados. ¿Valores esperados de qué, exactamente?
+
