9b6653d56753d31eab9eb03a544f755600129545
wparedes <wparedesgt@gmail.com>
2020-07-06 11:36:05 -0600

    examen español

---
+++ analisis/Semana02/Examen_Español.R
@@ -0,0 +1,72 @@
+#Examen en Español
+
+#Pregunta No. 1
+
+#¿Cuál es la varianza de la distribución del promedio de un dibujo IID de nn observaciones de una población con media \ muμ y varianza \ sigma ^ 2σ.
+
+#Pregunta No. 2
+
+
+#Suponga que las presiones sanguíneas diastólicas (PAD) para los hombres de 35 a 44 años normalmente se distribuyen con una media de 80 (mm Hg) y una desviación estándar de 10. Acerca de cuál es la probabilidad de que una persona aleatoria de 35 a 44 años tenga un PAD menor de 70?
+
+n <- seq(35:44)
+media <- 80
+desviacion <-10
+
+
+choose(80, 70) * .1 ^ 80 + choose(80, 80) * .1 ^ 8 
+
+pbinom((70:80), size = 8, prob = .5, lower.tail = FALSE)
+
+
+#32% 16% 8% 22%
+
+
+
+#Pregunta No. 3
+
+#El volumen cerebral para las mujeres adultas se distribuye normalmente con una media de aproximadamente 1.100 cc para las mujeres con una desviación estándar de 75 cc. ¿Qué volumen cerebral representa el percentil 95?
+
+qnorm(.95,mean = 1100, sd = 75)
+
+#1223.364
+
+#approximately 1175 approximately 977 approximately 1247 approximately 1223 
+
+
+#Pregunta No. 4
+
+#Consulte la pregunta anterior. El volumen cerebral para mujeres adultas es de aproximadamente 1.100 cc para mujeres con una desviación estándar de 75 cc. Considere la media muestral de 100 mujeres adultas aleatorias de esta población. ¿Cuál es el percentil 95 de la distribución de esa muestra?
+
+
+pnorm(1223.36)
+
+#approximately 1112 cc approximately 1115 cc approximately 1088 cc approximately 1110 cc
+
+#Pregunta No. 5
+
+#Lanzas una moneda justa 5 veces, ¿cuál es la probabilidad de obtener 4 o 5 caras?
+
+#12% 3% 19% 6%
+
+#Pregunta No.6
+
+
+#El índice de alteración respiratoria (IDR), una medida de la alteración del sueño, para una población específica tiene una media de 15 (eventos de sueño por hora) y una desviación estándar de 10. Normalmente no se distribuyen. ¿Da su mejor estimación de la probabilidad de que una muestra de IDR media de 100 personas sea entre 14 y 16 eventos por hora?
+
+
+#95% 68% 34% 47.5%
+
+
+#Pregunta No. 7
+
+#Considere una densidad uniforme estándar. La media de esta densidad es de .5 y la varianza es de 1/12. Si tomas muestras de 1,000 observaciones de esta distribución y tomas la media de la muestra, ¿qué valor esperarías que esté cerca?
+
+#0.5 0.25 0.10 0.75
+
+#Pregunta No. 8
+
+
+#Se supone que el número de personas que se presentan en una parada de autobús es Poisson con una media de 5 5 personas por hora. Miras la parada de autobús durante 3 horas. ¿Sobre cuál es la probabilidad de ver 10 o menos personas?
+
+#0.08 0.06 0.12 0.03
---
+++ analisis/Semana02/Examen_Ingles.R
@@ -0,0 +1,124 @@
+#Examen Semana 02
+
+.Pregunta 1
+What is the variance of the distribution of the average an IID draw of nn observations from a population with mean \muμ and variance \sigma^2σ 
+
+
+
+
+2.Pregunta 2
+Suppose that diastolic blood pressures (DBPs) for men aged 35-44 are normally distributed with a mean of 80 (mm Hg) and a standard deviation of 10. About what is the probability that a random 35-44 year old has a DBP less than 70?
+  
+  1 punto
+
+32%
+
+
+16%
+
+
+8%
+
+
+22%
+
+3.Pregunta 3
+Brain volume for adult women is normally distributed with a mean of about 1,100 cc for women with a standard deviation of 75 cc. What brain volume represents the 95th percentile?
+  
+  1 punto
+
+approximately 1175
+
+
+approximately 977
+
+
+approximately 1247
+
+
+approximately 1223
+
+4.Pregunta 4
+Refer to the previous question. Brain volume for adult women is about 1,100 cc for women with a standard deviation of 75 cc. Consider the sample mean of 100 random adult women from this population. What is the 95th percentile of the distribution of that sample mean?
+  
+  1 punto
+
+approximately 1112 cc
+
+
+approximately 1115 cc
+
+
+approximately 1088 cc
+
+
+approximately 1110 cc
+
+5.Pregunta 5
+You flip a fair coin 5 times, about what's the probability of getting 4 or 5 heads?
+
+1 punto
+
+12%
+
+
+3%
+
+
+19%
+
+
+6%
+
+6.Pregunta 6
+The respiratory disturbance index (RDI), a measure of sleep disturbance, for a specific population has a mean of 15 (sleep events per hour) and a standard deviation of 10. They are not normally distributed. Give your best estimate of the probability that a sample mean RDI of 100 people is between 14 and 16 events per hour?
+
+1 punto
+
+95%
+
+
+68%
+
+
+34%
+
+
+47.5%
+
+7.Pregunta 7
+Consider a standard uniform density. The mean for this density is .5 and the variance is 1 / 12. You sample 1,000 observations from this distribution and take the sample mean, what value would you expect it to be near?
+
+1 punto
+
+0.5
+
+
+0.25
+
+
+0.10
+
+
+0.75
+
+8.Pregunta 8
+The number of people showing up at a bus stop is assumed to be
+
+Poisson with a mean of 5 5 people per hour. You watch the bus
+
+stop for 3 hours. About what's the probability of viewing 10 or fewer people?
+  
+  1 punto
+
+0.08
+
+
+0.06
+
+
+0.12
+
+
+0.03
+
