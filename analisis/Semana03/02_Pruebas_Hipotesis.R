1da09bd73dea5f4ccbb2540d1619b8dd66d460b2
wparedes <wparedesgt@gmail.com>
2020-07-09 17:27:49 -0600

    Cambios y nueva pagina

---
+++ analisis/Semana03/02_Pruebas_Hipotesis.R
@@ -0,0 +1,4 @@
+#Intro Pruebas de Hipotesis
+
+#Decidir entre dos hipótesis es una actividad central en el descubrimiento científico. La prueba estadística de hipótesis es el marco inferencial formal en torno a la elección entre hipótesis.
+
